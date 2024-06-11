ba45388e211cf0008e8d177d9528bd2db476e7b5
agiatikazinat <robotproject333@gmail.com>
2024-06-11 11:36:42 -0400

    redo bookstore

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import Cart from "./components/Cart";
+import NavBar from "./components/NavBar";
+import Home from "./components";
+import {
+  BrowserRouter as Router, 
+  Routes, 
+  Route,
+} from "react-router-dom";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [items, setItems] = useState([]);
+  const [products, setProducts] = useState([]);
+  
+  const addToCart = (idToAdd) => {
+    let newItems = [...items, idToAdd];
+    setItems(newItems);
+  };
+
+  const removeFromCart = (idToRemove) => {
+      let newItems = items.filter((id) => id !== idToRemove);
+      setItems(newItems);
+  };
+
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+    }
+    return array;
+  };
+
+  useEffect(() => {
+    fetch("/data/products.json").then((response) =>
+      response
+        .json()
+        .then((products) => shuffleArray(products))
+        .then((products) => {
+          setProducts(products);
+        })
+    );
+  }, []);
+
+  
+
+  const getProduct = (products, item) => {
+      return products.find((product) => item === product.id);
+  };
+
+
+  let itemDetails = items.map((id) => getProduct(products, id));
+
+  return(
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home addToCart={addToCart} removeFromCart={removeFromCart} products={products} items={items}/>} />;
+
+        <Route path="/cart" element={<Cart removeFromCart={removeFromCart} inCart={itemDetails}/>} />;
+
+        <Route path="/signup" />;
+
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/CartItem.css.js
@@ -0,0 +1,15 @@
+export default {
+    cartItem: {},
+    table: {
+        border: "1px solid black",
+        width: "100%",
+    },
+    td: {
+        border: "1px solid black",
+        width: "33%",
+    }, 
+    th : {
+        border: "1px solid black",
+        width: "100%",
+    },
+};
